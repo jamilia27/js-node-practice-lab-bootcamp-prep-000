da8471c1e72b8887cfbfac249340ce269483b9eb
katie.weber@powerhrg.com <katie.weber@powerhrg.com>
2018-04-16 15:24:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Katie"
-var height = "5'10"
+var height = 5
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1)
+console.log(height + 1)
